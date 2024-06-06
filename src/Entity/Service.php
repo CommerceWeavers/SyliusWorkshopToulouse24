5783f6e1d74f8df040b5121132461cb582238b03
Łukasz Chruściel <lchrusciel@gmail.com>
2024-06-06 10:41:06 +0200

    Define service as basic resource

--- src/Entity/Service.php
+++ src/Entity/Service.php
@@ -9,13 +9,19 @@ use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\Table;
+use Sylius\Component\Resource\Model\ResourceInterface;
 
 #[Entity]
 #[Table(name: 'acme_sylius_awesome_service')]
-class Service
+class Service implements ResourceInterface
 {
     #[Id]
     #[GeneratedValue(strategy: 'AUTO')]
     #[Column(type: 'integer')]
     private $id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
 }
